ea89ebf0746797ac8de662560ce8d1eeb8dc2aeb
Pedro Gabriel <pedrpg5@gmail.com>
2023-10-09 09:06:00 -0300

    Melhorias

--- alura-sapce/galeria/views.py
+++ alura-sapce/galeria/views.py
@@ -8,3 +8,12 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id);
     return render(request, 'galeria/imagem.html', {"foto": foto})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    
+    if "buscar" in request.GET:
+        nome = request.GET['buscar']
+        if nome:
+            fotografias = fotografias.filter(nome__icontains=nome)
+    return render(request, 'galeria/buscar.html', {"cards": fotografias});
